8428712e932c3ad0222bd5b0b5558f993a403d9b
Lucas <llovizzio@gmail.com>
2025-08-29 18:40:20 -0300

    muevo el main de EJ1 a su respectiva paquete

--- src/EJ1/MainEJ1.java
+++ src/EJ1/MainEJ1.java
@@ -1,10 +1,10 @@
-import EJ1.*;
+package EJ1;
 
 import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MainEJ1 {
     public static void main(String[] args) {
 
         // Crear artículos
